20ed4b46271a4727444e733e438dc4011d0a7f85
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-06-23 07:40:50 +0200

    Update Form.styled.jsx

--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -1,35 +1,11 @@
-// import { Form, Field } from 'formik';
-// import styled from '@emotion/styled';
+import { Form, Field } from 'formik';
+import styled from '@emotion/styled';
 
-// export const MainForm = styled(Form)`
-//   position: relative;
-//   max-width: 300px;
-//   padding: 20px;
-//   border: ${props => `1px solid ${props.theme.colors.black}`};
-//   border-radius: 4px;
-// `;
-
-// export const Label = styled.label`
-//   display: block;
-//   font-size: 18px;
-// `;
-// export const ErrorText = styled.p`
-//   color: red;
-// `;
-
-// export const InputName = styled(Field)`
-//   margin-bottom: 10px;
-// `;
-// export const InputNumber = styled(Field)``;
-
-import { Field } from 'formik';
-import styled from 'styled-components';
-
-export const MainForm = styled.form`
+export const MainForm = styled(Form)`
   position: relative;
   max-width: 300px;
   padding: 20px;
-  ${'' /* border: ${props => `1px solid ${props.theme.colors.black}`}; */}
+  border: black;
   border-radius: 4px;
 `;
 
@@ -37,7 +13,6 @@ export const Label = styled.label`
   display: block;
   font-size: 18px;
 `;
-
 export const ErrorText = styled.p`
   color: red;
 `;
@@ -45,5 +20,4 @@ export const ErrorText = styled.p`
 export const InputName = styled(Field)`
   margin-bottom: 10px;
 `;
-
 export const InputNumber = styled(Field)``;
